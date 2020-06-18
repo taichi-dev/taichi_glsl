0e6f74120938ca5e0573234fc6a65c3f81c833a2
archibate <1931127624@qq.com>
2020-06-18 10:48:18 +0800

    Add inputs are welcomed

--- taichi_glsl/version.py
+++ taichi_glsl/version.py
@@ -2,3 +2,4 @@ version = (0, 0, 4)
 taichi_version = (0, 6, 8)
 
 print(f'[TaiGLSL] version {".".join(map(str, version))}')
+print(f'[TaiGLSL] Inputs are welcomed at https://github.com/taichi-dev/taichi')
