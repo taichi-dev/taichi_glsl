8d113f776d58c4c49e91eee248461559a09a3ca1
archibate <1931127624@qq.com>
2020-06-21 16:53:39 +0800

    hack ti.Vector.var and ti.static

--- taichi_glsl/hack.py
+++ taichi_glsl/hack.py
@@ -19,8 +19,25 @@ def _deco_fix(old_deco):
     return decorator
 
 
-# A hotfix for taichi<=0.6.10 not preserving function info.
+# A hotfix for taichi<0.6.11 not preserving function info.
 # We needs this hijack function to make documentation strings work.
 # We can remove this after taichi-dev/taichi#1233 merged in v0.6.11.
 if ti.__version__ < (0, 6, 11):
     ti.func = _deco_fix(ti.func)
+
+if not hasattr(ti.Vector, 'var'):
+    ti.Vector.var = ti.Vector
+
+# Make ti.static support Taichi classes:
+ti_static = ti.static
+def _ts_static(x, *xs):
+    def _static(x):
+        if ti.is_taichi_class(x):
+            return x
+        else:
+            return ti_static(x)
+    if len(xs) == 0:
+        return _static(x)
+    return [_static(x)] + [_static(_) for _ in xs]
+
+ti.static = _ts_static
