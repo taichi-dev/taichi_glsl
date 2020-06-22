f080b34668d7d7f896321628c06606592b182075
archibate <1931127624@qq.com>
2020-06-22 15:48:12 +0800

    add variable and get_tensor_members TaichiClass

--- taichi_glsl/odop.py
+++ taichi_glsl/odop.py
@@ -12,6 +12,7 @@ class TaichiClass:
         self.entries = entries
 
     @classmethod
+    @ti.python_scope
     def var(cls, *args, **kwargs):
         var_list = cls._var(*args, **kwargs)
         if not isinstance(var_list, (list, tuple)):
@@ -26,9 +27,24 @@ class TaichiClass:
     def _subscript(self, I):
         return self.__class__(*(e[I] for e in self.entries))
 
+    @ti.taichi_scope
     def subscript(self, I):
         return self._subscript(I)
 
     @ti.func
     def loop_range(self):
-        return self.entries[0]
+        return self.entries[0].loop_range()
+
+    def get_tensor_members(self):
+        ret = []
+        for e in self.entries:
+            if hasattr(e, 'get_tensor_members'):
+                e = e.get_tensor_members()
+            else:
+                e = [e]
+            ret += e
+        return ret
+
+    @ti.taichi_scope
+    def variable(self):
+        return self.__class__(*(e.variable() for e in self.entries))
