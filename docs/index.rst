801feb6b349da6e8391495fcab723ad8a25a3e16
archibate <1931127624@qq.com>
2020-07-03 16:03:26 +0800

    update desc

--- docs/index.rst
+++ docs/index.rst
@@ -6,9 +6,9 @@ Taichi GLSL is an extension library of the `Taichi Programming Language <https:/
 1. Handy scalar functions like ``clamp``, ``smoothstep``, ``mix``, ``round``.
 2. GLSL-alike vector functions like ``normalize``, ``distance``, ``reflect``.
 3. Well-behaved random generators including ``randUnit3D``, ``randNDRange``.
-4. Possible Taichi BUG hotfixes that are not yet released in it's cycle.
 5. Handy vector and matrix initializer: ``vec`` and ``mat``.
 6. Handy vector component shuffle accessor like ``v.xy``.
+4. Handy field sampler including ``bilerp`` and ``sample``.
 7. Useful physics helper functions like ``boundReflect``.
 8. Shadertoy-alike inputed GUI base class ``Animation``.
 
--- setup.py
+++ setup.py
@@ -12,11 +12,14 @@ Taichi GLSL is an extension library of the `Taichi Programming Language <https:/
 1. Handy scalar functions like ``clamp``, ``smoothstep``, ``mix``, ``round``.
 2. GLSL-alike vector functions like ``normalize``, ``distance``, ``reflect``.
 3. Well-behaved random generators including ``randUnit3D``, ``randNDRange``.
-4. Possible Taichi BUG hotfixes that are not yet released in it's cycle.
 5. Handy vector and matrix initializer: ``vec`` and ``mat``.
 6. Handy vector component shuffle accessor like ``v.xy``.
+4. Handy field sampler including ``bilerp`` and ``sample``.
 7. Useful physics helper functions like ``boundReflect``.
 8. Shadertoy-alike inputed GUI base class ``Animation``.
+
+<[Clike me for documentation] (https://taichi-glsl.readthedocs.io)>_
+<[Clike me for GitHub repo] (https://github.com/taichi-dev/taichi_glsl)>_
 '''
 classifiers = [
     'Intended Audience :: Developers',
