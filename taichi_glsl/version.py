dcdfef9e57865dd5a9bf48b9283f41ce85ba6b01
archibate <1931127624@qq.com>
2020-07-05 00:42:27 +0800

    fix typo 2

--- taichi_glsl/version.py
+++ taichi_glsl/version.py
@@ -2,4 +2,4 @@ version = (0, 0, 7)
 taichi_version = (0, 6, 14)
 
 print(f'[TaiGLSL] version {".".join(map(str, version))}')
-print(f'[TaiGLSL] Inputs are welcomed at https://github.com/taichi-dev/taichi')
+print(f'[TaiGLSL] Inputs are welcomed at https://github.com/taichi-dev/taichi_glsl')
