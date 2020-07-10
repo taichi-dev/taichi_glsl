8085b35833563e3d0a5ae975b81f9a05c901c6f7
archibate <1931127624@qq.com>
2020-07-10 17:38:03 +0800

    up

--- examples/fractal.py
+++ examples/fractal.py
@@ -8,7 +8,7 @@ ti.init()
 class MyAnimation(tl.Animation):
     def on_init(self, n=512):
         self.n = n
-        self.title = 'Fractal'
+        self.title = 'Julia Set'
         self.img = ti.var(ti.f32, (self.n * 2, self.n))
         self.colormap = cm.get_cmap('magma')
         self.define_input()
@@ -19,10 +19,10 @@ class MyAnimation(tl.Animation):
             c = tl.Complex(-0.8, ti.cos(self.iTime) * 0.2)
             z = tl.Complex(p / self.n - tl.vec(1, 0.5)) @ 2
             iterations = 0
-            while z.mag2 < 4 and iterations < 50:
+            while z.mag2 < 4 and iterations < 100:
                 z = z @ z + c
                 iterations += 1
-            self.img[p] = 1 - iterations * 0.02
+            self.img[p] = 1 - iterations * 0.01
 
 
 MyAnimation().start()
