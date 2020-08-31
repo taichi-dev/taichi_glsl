b2a1b1e6fb3a552b413cf5fd5be0c1547ca1464a
archibate <1931127624@qq.com>
2020-08-31 14:55:46 +0800

    remove useless banner

--- taichi_glsl/version.py
+++ taichi_glsl/version.py
@@ -2,6 +2,3 @@ version = (0, 0, 9)
 taichi_version = (0, 6, 28)
 
 print(f'[TaiGLSL] version {".".join(map(str, version))}')
-print(
-    f'[TaiGLSL] Inputs are welcomed at https://github.com/taichi-dev/taichi_glsl'
-)
