f3d5d453d3f50b24a77b801da7fbed3db4cae59a
Jack12xl <jack12xieling@163.com>
2020-12-09 00:53:39 +0800

    add trilerp

--- taichi_glsl/sampling.py
+++ taichi_glsl/sampling.py
@@ -94,6 +94,56 @@ def bilerp(field: ti.template(), P):
 
 
 @ti.func
+def trilerp(field: ti.template(), P):
+    '''
+    Tilinear sampling an 3D field with a real index.
+
+    :parameter field: (3D Tensor)
+        Specify the field to sample.
+
+    :parameter P: (3D Vector of float)
+        Specify the index in field.
+
+    :note:
+        If one of the element to be accessed is out of `field.shape`, then
+        `Tilerp` will automatically do a clamp for you, see :func:`sample`.
+
+        Syntax ref : https://en.wikipedia.org/wiki/Trilinear_interpolation.
+
+    :return:
+        The return value is calcuated as::
+
+            I = int(P)
+            w0 = ts.fract(P)
+            w1 = 1.0 - w0
+            c00 = ts.sample(field, I + ts.D.yyy) * w1.x + ts.sample(field, I + ts.D.xyy) * w0.x
+            c01 = ts.sample(field, I + ts.D.yyx) * w1.x + ts.sample(field, I + ts.D.xyx) * w0.x
+            c10 = ts.sample(field, I + ts.D.yxy) * w1.x + ts.sample(field, I + ts.D.xxy) * w0.x
+            c11 = ts.sample(field, I + ts.D.yxx) * w1.x + ts.sample(field, I + ts.D.xxx) * w0.x
+
+            c0 = c00 * w1.y + c10 * w0.y
+            c1 = c01 * w1.y + c11 * w0.y
+
+            return c0 * w1.z + c1 * w0.z
+
+        .. where D = vec(1, 0, -1)
+    '''
+    I = int(P)
+    w0 = ts.fract(P)
+    w1 = 1.0 - w0
+
+    c00 = ts.sample(field, I + ts.D.yyy) * w1.x + ts.sample(field, I + ts.D.xyy) * w0.x
+    c01 = ts.sample(field, I + ts.D.yyx) * w1.x + ts.sample(field, I + ts.D.xyx) * w0.x
+    c10 = ts.sample(field, I + ts.D.yxy) * w1.x + ts.sample(field, I + ts.D.xxy) * w0.x
+    c11 = ts.sample(field, I + ts.D.yxx) * w1.x + ts.sample(field, I + ts.D.xxx) * w0.x
+
+    c0 = c00 * w1.y + c10 * w0.y
+    c1 = c01 * w1.y + c11 * w0.y
+
+    return c0 * w1.z + c1 * w0.z
+
+
+@ti.func
 def superSample2x2(fieldFunc: ti.template(), P, dx=1):
     dD = dx / 2 * D
     return (fieldFunc(P + dD.yy) + fieldFunc(P + dD.yz) +
